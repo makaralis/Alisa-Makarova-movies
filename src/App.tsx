97b47a1585a581b27f7dfbe8505e039cdfd2ea76
Alisa <alisa@jonnix.co>
2021-12-03 17:22:40 +0200

    added Container to the App

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom'
 import HomePage from './pages/Home';
 import FavoritesPage from './pages/Favorites';
+import { Container } from './styles/global';
 
 const App = () => {
   return (
-    <>
+    <Container>
      <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="favorite-movies/*" element={<FavoritesPage />} />
       </Routes>
-    
-    </>
+    </Container>
   );
 }
 
---
+++ src/styles/global.tsx
@@ -0,0 +1,9 @@
+import { styled } from "@mui/system";
+
+export const Container = styled('div')(({theme}) => `
+    background: #1e1e46;
+    height: 100%;
+    position: fixed;
+    width: 100%;
+    overflow-y: scroll;
+`,)
