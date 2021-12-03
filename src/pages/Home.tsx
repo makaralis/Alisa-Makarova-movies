edcefef0ad352e1c444370b88b330e959c49e989
Alisa <alisa@jonnix.co>
2021-12-03 17:23:44 +0200

    created getMovies function

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,31 @@
+import { useCallback, useEffect, useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+
 const HomePage = () => {
-    return <></>
+    const [moviesData, setMoviesData] = useState();
+
+    const getMovies = useCallback(async () => {
+        try {
+            const res = await axios.get("https://swapi.dev/api/films");
+
+            if (res && res.data.results) {
+                setMoviesData(res.data.results);
+            }
+        }
+        catch (e) {
+            toast.error("Error while fetching movies data")
+        }
+    }, [])
+
+    useEffect(() => {
+        getMovies();
+    },[getMovies]);
+
+    console.log(moviesData);
+
+    return (<>
+    </>)
 }
 
 
