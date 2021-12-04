dce7091067805dbca5a1720e4170a1bd8b23f3da
Alisa <alisa@jonnix.co>
2021-12-04 09:26:01 +0200

    added loading to the App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,50 @@
-import './App.css';
-import { Route, Routes } from 'react-router-dom'
+import { useCallback, useEffect, useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { Route, Routes } from 'react-router-dom';
+import { CircularProgress, Grid, Typography } from '@mui/material';
+import { useTheme } from "@mui/system";
 import HomePage from './pages/Home';
 import FavoritesPage from './pages/Favorites';
 import { Container } from './styles/global';
 
 const App = () => {
+  const [moviesData, setMoviesData] = useState();
+  const [loading, setLoading] = useState<Boolean>(true);
+  const theme = useTheme();
+
+
+  const getMovies = useCallback(async () => {
+      try {
+          const res = await axios.get("https://swapi.dev/api/films");
+
+          if (res && res.data.results) {
+              setMoviesData(res.data.results);
+              setLoading(false);
+          }
+      }
+      catch (e) {
+          toast.error("Error while fetching movies data");
+          setLoading(false);
+      }
+  }, [])
+
+  useEffect(() => {
+      getMovies();
+  }, [getMovies]);
+
+
   return (
     <Container>
-     <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="favorite-movies/*" element={<FavoritesPage />} />
-      </Routes>
+      {loading ? <Grid container
+              direction="column"
+              justifyContent="center"
+              alignItems="center"><CircularProgress size={35} />
+              <Typography sx={{marginTop: theme.spacing(2)}} variant='h6'>Loading the data...</Typography></Grid> : 
+      <Routes>
+          <Route path="/" element={<HomePage moviesData={moviesData}/>} />
+          <Route path="favorite-movies/*" element={<FavoritesPage />} />
+      </Routes>}
     </Container>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,17 @@
-import { useCallback, useEffect, useState } from "react";
-import axios from "axios";
-import { toast } from "react-toastify";
+
+import React from "react";
 import { Grid } from "@mui/material";
 import MoviesContent from "../components/MoviesContent";
+import { useTheme } from '@mui/system';
 
-const HomePage = () => {
-    const [moviesData, setMoviesData] = useState();
+interface HomePageProps {
+    moviesData: any;
+}
 
-    const getMovies = useCallback(async () => {
-        try {
-            const res = await axios.get("https://swapi.dev/api/films");
+const HomePage: React.FC<HomePageProps> = ({ moviesData }) => {
+    const theme = useTheme();
 
-            if (res && res.data.results) {
-                setMoviesData(res.data.results);
-            }
-        }
-        catch (e) {
-            toast.error("Error while fetching movies data")
-        }
-    }, [])
-
-    useEffect(() => {
-        getMovies();
-    }, [getMovies]);
-
-
-    return (<Grid container spacing={2}>
+    return (<Grid container spacing={2} sx={{margin: theme.spacing(1,3)}}>
         <MoviesContent moviesData={moviesData}/>
     </Grid>)
 }
