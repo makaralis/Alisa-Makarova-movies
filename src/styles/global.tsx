e8765f93c89ef0a4eef942267aa72b469c02f060
Alisa <alisa@jonnix.co>
2021-12-04 17:43:08 +0200

    fixed the size of the background

--- src/styles/global.tsx
+++ src/styles/global.tsx
@@ -2,6 +2,10 @@ import { styled } from "@mui/system";
 
 export const Container = styled('div')(({theme}) => `
     background-image: url('/star-wars.jpg');
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
     height: 100%;
     position: fixed;
     width: 100%;
